5ea815371b3afe487d3cd01494d9dbbd827cdb5c
Yicheng Fu <yichengfu@DNa8086c5.SUNet>
2025-01-25 14:37:05 -0800

    update run_cot

--- run_cot.sh
+++ run_cot.sh
@@ -1,8 +1,8 @@
 # Experiment
 echo Experiment: Chain of Thought
 python experiments/run_simple_experiment.py \
-  -m gpt-4o-mini \
-  -r 1 \
+  -m gpt-4o \
+  -r 20 \
   -s 1234 \
   -a "Be sure to use the chain of thought tool to perform structured reasoning for negotiation decisions. **Do NOT accept a trade without performing chain of thought reasoning**." \
   -d double_script \
